9c12dffeaa340137d8c4e232e4e3083205dbfba5
Alessandro <descovi@gmail.com>
2016-03-06 09:57:39 +0100

    ignora node modules

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-require('net').createServer(function (socket) {
-    console.log("connected");
+#!/usr/bin/env node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 
-    socket.on('data', function (data) {
-        console.log(data.toString());
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log('Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: true
+});
+
+
+wsServer.on('request', function(request) {
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log( 'Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
     });
-}).listen(8080);
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
