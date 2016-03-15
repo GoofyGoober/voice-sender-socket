19ea5eb23d7897f52291d2a3594796136ab00088
Alessandro <descovi@gmail.com>
2016-03-15 15:49:00 +0100

    incluso jquery

--- client/server.js
+++ client/server.js
@@ -1,24 +1,25 @@
 var connection;
-connection = new WebSocket("ws://"+getIpFromInput())
-// Log messages from the server
-connection.onmessage = function (e) {
-  console.log('Server: ' + e.data);
-};
-connection.onerror = function (e) {
-  console.log('Server: ' + e.data);
-};
-connection.onopen = function(e) {
-    console.log("Connection established!");
-    connection.send('Hello Me!');
-};
-
-
-
-function getIpFromInput(){
-  return document.getElementById('ip').value
-}
+connection = new WebSocket("ws://"+$('#ip').val())
+setupConnection();
 
 
 function changeIp(e){
-  connection = new WebSocket("ws://"+getIpFromInput())
+  connection = new WebSocket("ws://"+$('#ip').val())
+  setupConnection();
+}
+
+function setupConnection(){
+  // Log messages from the server
+  connection.onmessage = function (e) {
+    console.log('Server: ' + e.data);
+    $('#status-server').html('Server: '+e.data)
+  };
+  connection.onerror = function (e) {
+    console.log('Server: ' + e.data);
+    $('#status-server').html('Server: '+e.data)
+  };
+  connection.onopen = function(e) {
+    console.log("Connection established!");
+    connection.send('Hello Me!');
+  };
 }
